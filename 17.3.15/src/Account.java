cdb7351956c8fd6038cf47591f7b95c7718db092
KAU <KAU@KAU-PC>
2017-03-27 00:54:21 +0900

    3.27 수정

--- 17.3.15/src/Account.java
+++ 17.3.15/src/Account.java
@@ -2,40 +2,45 @@ import java.util.Scanner;
 
 public class Account {
 	
-	private double account1 = 50.00;
-	private double account2 = 0.00;
+	private double balance;
+	private double account1;
+	private double account2;
 	
-	public void debit() {
+	//adds money to an account
+	public void credit() { 
 		
-		System.out.printf("Enter withdrawal amount for account1 : ");
+		// System.out.printf("Input account1 : ");
 		Scanner input = new Scanner(System.in);
-		double num1 = input.nextDouble();
+		double credit1 = input.nextDouble();
 		
-		if(account1>=num1) {
-			System.out.printf("subtracting %f from account1 balance \n", num1);
-			account1 = account1 - num1;
-			System.out.printf("account1 balance : %f\n", account1);
-		}
-		else {
-			System.out.printf("Debit amount exceeded account balance.\n");
-		}
+		//System.out.printf("Input account2 : ");
+		double credit2 = input.nextDouble();
 		
-		System.out.printf("Enter withdrawal amount for account2 : ");
-		double num2 = input.nextDouble();
+		//System.out.printf("account1 balance : %f\n", credit1);
+		//System.out.printf("account2 balance : %f\n", credit2);
+		}
+
+	//withdraws money from an account
+	public void debit() {
+
+		//System.out.printf("Enter withdrawal amount for account1 : ");
+		Scanner input = new Scanner(System.in);
+		double debit1 = input.nextDouble();
 		
-		if(account2>=num2) {
-			System.out.printf("subtracting %f from account2 balance \n", num2);
-			account2 = account2 - num2;
-			System.out.printf("account1 balance : %f\n", account2);
-		}
-		else {
-			System.out.printf("Debit amount exceeded account balance.\n");
-		}
+		//System.out.printf("Enter withdrawal amount for account2 : ");
+		double debit2 = input.nextDouble();
 	}
 	
-	public Account(double b){
-		account1 = b;
+	//shows the balance of an account
+	public void balance() {
+		
 	}
+
+	
+	/*	public Account(double b){
+		account1 = b;
+	}*/
+	
 	
 		
 }	
--- 17.3.15/src/AccountTest.java
+++ 17.3.15/src/AccountTest.java
@@ -3,8 +3,23 @@
 public class AccountTest {
 
 	public static void main(String[] args) {
+	
+		double credit1 = 0, credit2 = 0;
+		double debit1 = 0, debit2 = 0;
+		
 		Account account = new Account();
+		
+		System.out.printf("Input account1 : ");
+		System.out.printf("Input account2 : ");
+		account.credit();
+		System.out.printf("account1 balance : %f\n", credit1);
+		System.out.printf("account2 balance : %f\n", credit2);
+		
+		System.out.printf("Enter withdrawal amount for account1 : ");
+		System.out.printf("Enter withdrawal amount for account2 : ");
 		account.debit();
 		
+		System.out.printf("subtracting %f from account1 balance \n", debit1);
+		account.balance();
 	}
 }
