c6c3f9b2732f9e9e0388714796e6431812d0b578
TaeHeon0711 <TaeHeon0711>
2017-06-26 13:58:32 +0900

    6.26 앱 작동 안되서 자바로 부분수정해서 올림

--- TermProject/src/TermProject/RCPtest.java
+++ TermProject/src/TermProject/RCPtest.java
@@ -10,7 +10,7 @@ public class RCPtest {
 	private static int comnum;
 	private static int coin = 20;
 	private static int getcoin;
-
+	
 	static void coinCheck() { //user의 코인을 check하는 method
 		
 		System.out.println("현재" + coin + "개의 코인을 가지고 있습니다.");
@@ -25,26 +25,32 @@ public class RCPtest {
 		
 	static void userInput() { //user한테 가위바위보 중 한가지를 입력받는 method
 		
-		Scanner scanner = new Scanner(System.in);	
+		Scanner scanner = new Scanner(System.in);
+		
 		if(coin > 0) {
-			coin = coin - 1;
 			System.out.printf("가위바위보에서 낼 것을 선택하세요(0. 가위, 1. 바위, 2. 보) : ");
 			usernum = scanner.nextInt();
+
 			if(usernum == 0) {
 				System.out.println("당신은 가위를 선택하였습니다.");
+				comOutput();
+				result();
 			}
 			else if(usernum == 1) {
 				System.out.println("당신은 바위를 선택하였습니다.");
+				comOutput();
+				result();
 			}
 			else if(usernum == 2) {
 				System.out.println("당신은 보를 선택하였습니다.");
+				comOutput();
+				result();
 			}
 			else {
-				System.out.println("잘못 된 입력을 하였습니다."); //예외처리
-				return;
+				System.out.println("잘못 된 입력을 하였습니다. 0, 1, 2 중에 입력하세요.");
 			}
 		}
-}
+	}
 	
 	static void comOutput () { //computer가 가위바위보 중 무엇을 내는지 출력해주는 method
 		
@@ -70,58 +76,37 @@ public class RCPtest {
 		getcoin = random.nextInt(20) + 1;
 		System.out.println(getcoin + "개의 코인을 얻었습니다.");
 		coin = coin + getcoin;
-		System.out.println("현재" + coin + "개의 코인을 가지고 있습니다.");
-		
+		System.out.println("총" + coin + "개의 코인을 가지고 있습니다.");
+	
 	}
 	
-	public static void main(String args[]) {
-		
-		int gamenum;
-		Scanner scanner = new Scanner(System.in);
-		Random random = new Random();
-		
-		while(true) {
-			coinCheck();
-			System.out.printf("게임을 진행하시겠습니까? (1. 진행, 2. 나가기)");
-			gamenum = scanner.nextInt();
-			
-			if(gamenum == 1) {
-				userInput();
-				comOutput();
-
-				if(usernum == comnum) {
-					System.out.println("비겼습니다!");
-				}
-				else if((usernum == 0 && comnum == 1) || (usernum == 1 && comnum == 2) || (usernum == 2 && comnum == 0)) {
-					System.out.println("패배하였습니다!");
-				}
-				else if((usernum == 1 && comnum == 0) || (usernum == 2 && comnum == 1) || (usernum == 0 && comnum == 2)) {
-					System.out.println("승리하였습니다!");
-					getCoin();
-			}
-			else if(gamenum == 2) {
-				break;
-			}
-			else {
-				System.out.println("잘못입력하였습니다."); //예외처리
-			}
-		}
-		}
-	}
-}
-/*		coinCheck();
-		userInput();
-		comOutput();
-
+	static void result() { //게임의 결과를 알려주는 method
 		if(usernum == comnum) {
 			System.out.println("비겼습니다!");
 		}
 		else if((usernum == 0 && comnum == 1) || (usernum == 1 && comnum == 2) || (usernum == 2 && comnum == 0)) {
 			System.out.println("패배하였습니다!");
+			coin = coin - 1;
 		}
 		else if((usernum == 1 && comnum == 0) || (usernum == 2 && comnum == 1) || (usernum == 0 && comnum == 2)) {
 			System.out.println("승리하였습니다!");
-			
 			getCoin();
 		}
-	}*/
+	}
+	
+	public static void main(String args[]) {
+		
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+		
+		while(true) {
+			coinCheck();
+			try {
+				userInput();
+			}
+			catch(InputMismatchException ime) {
+				System.out.println("문자열을 입력하였습니다. 0, 1, 2 중에서 입력하세요");
+			}
+		}
+	}
+}
